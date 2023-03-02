b7e6288db27f63294dc79c2a487db34478659436
otienoonyango2000 <otienoonyango2000@gmail.com>
2023-03-03 02:02:37 +0300

    second commit tvland

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,10 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actor, through: :characters
+  belongs_to :network
+
+#   def actor_list
+#     self.actors.map (&:full_name)
+
+#     end    
 end
